3a56c45314bfd4a419e6bd07c3ddc07e3714972d
paulbqnt <paul.boquant@gmail.com>
2024-06-23 11:52:03 +0200

    new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import './App.css';
-import { MuiButton } from './components/MuiButton';
+// import { MuiButton } from './components/MuiButton';
+import {MuiTextField} from "./components/MuiTextField";
 // import { MuiTypography } from "./components/MuiTypography";
-// https://www.youtube.com/watch?v=BS2hOxbXMgw&list=PLC3y8-rFHvwh-K9mDlrrcDywl7CeVL2rO&index=7
+import { MuiSelect } from "./components/MuiSelect"
+import {MuiRadioButton} from "./components/MuiRadioButton";
+import {FormHelperText} from "@mui/material";
+
+// https://www.youtube.com/watch?v=9L0QhB-6xBI&list=PLC3y8-rFHvwh-K9mDlrrcDywl7CeVL2rO&index=10
+
+
 function App() {
   return (
     <div className="App">
       {/*<MuiTypography />*/}
-        <MuiButton />
-
+      {/*  <MuiButton />*/}
+      {/*  <MuiTextField/>*/}
+      {/*  <MuiSelect/>*/}
+      <MuiRadioButton/>
     </div>
   );
 }
