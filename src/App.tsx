f4a2732f6fd2903b9016ddabe586f2f460dff319
Paul <68791915+paulbqnt@users.noreply.github.com>
2024-07-07 18:56:39 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,40 @@
 import './App.css';
 // import { MuiButton } from './components/MuiButton';
-import {MuiTextField} from "./components/MuiTextField";
-// import { MuiTypography } from "./components/MuiTypography";
+// import {MuiTextField} from "./components/MuiTextField";
+import { MuiTypography } from "./components/MuiTypography";
 import { MuiSelect } from "./components/MuiSelect"
-import {MuiRadioButton} from "./components/MuiRadioButton";
-import {FormHelperText} from "@mui/material";
-import {MuiCheckbox} from "./components/MuiCheckbox";
-import {MuiRating} from "./components/MuiRating";
-import {MuiAutocomplete} from "./components/MuiAutocomplete";
-import {MuiLayout} from "./components/MuiLayout";
+// import {MuiRadioButton} from "./components/MuiRadioButton";
+// import {FormHelperText} from "@mui/material";
+// import {MuiCheckbox} from "./components/MuiCheckbox";
+// import {MuiRating} from "./components/MuiRating";
+// import {MuiAutocomplete} from "./components/MuiAutocomplete";
+// import {MuiLayout} from "./components/MuiLayout";
 // import {MuiSwitch} from "./components/MuiSwitch";
 // https://www.youtube.com/watch?v=e7lRxspNIdU&list=PLC3y8-rFHvwh-K9mDlrrcDywl7CeVL2rO&index=15
+import ReactDOM from 'react-dom';
+import {MuiNavbar } from "./components/MuiNavbar";
+import { MuiTable } from "./components/MuiTable";
+// import DataTable from "./components/DataTable";
+
+
 function App() {
   return (
-    <div className="App">
-      {/*<MuiTypography />*/}
-      {/*  <MuiButton />*/}
-      {/*  <MuiTextField/>*/}
-      {/*  <MuiSelect/>*/}
-      {/*<MuiRadioButton/>*/}
-      {/*  <MuiCheckbox/>*/}
-      {/*<MuiSwitch/>*/}
-      {/*  <MuiRating/>*/}
-      {/*<MuiAutocomplete/>*/}
-        <MuiLayout/>
-    </div>
+
+      <div className="App">
+        {/*<MuiTypography />*/}
+        {/*  <MuiButton />*/}
+        {/*  <MuiTextField/>*/}
+        {/*  <MuiSelect/>*/}
+        {/*<MuiRadioButton/>*/}
+        {/*  <MuiCheckbox/>*/}
+        {/*<MuiSwitch/>*/}
+        {/*  <MuiRating/>*/}
+        {/*<MuiAutocomplete/>*/}
+        {/*<MuiLayout/>*/}
+          <MuiNavbar/>
+          <MuiTable/>
+          {/*<DataTable/>*/}
+      </div>
   );
 }
 
